6321742852bcaf5762cf0ad1700681da8a86e3fc
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2021-11-30 17:15:14 +0100

    prueba

---
+++ send_telegram/index.js
@@ -0,0 +1,7 @@
+const core = require('@actions/core');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.sendMessage(process.env.ID_CHAT, "Workflow ejecutado correctamente tras el último commit. Saludos " + core.getInput('who-to-greet'));
